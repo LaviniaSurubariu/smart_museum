a499064a7a184fa0d979c022725dcb8a97695c7a
LaviniaSurubariu <surubariu.lavinia@gmail.com>
2024-05-01 02:45:31 +0300

    add ProfileMenuWidget.dart

---
+++ lib/presentation/utils/ProfileMenuWidget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+
+
+
+class ProfileMenuWidget extends StatelessWidget {
+  const ProfileMenuWidget({
+    Key? key,
+    required this.title,
+    required this.icon,
+    required this.onPress,
+    this.endIcon = true,
+    this.textColor,
+  }) : super(key: key);
+
+  final String title;
+  final IconData icon;
+  final VoidCallback onPress;
+  final bool endIcon;
+  final Color? textColor;
+
+  @override
+  Widget build(BuildContext context) {
+
+    const Color iconColor = Colors.black12;
+
+    return ListTile(
+      onTap: onPress,
+      leading: Container(
+        width: 40,
+        height: 40,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          color: iconColor.withOpacity(0.1),
+        ),
+        child: Icon(icon, color: Colors.black),
+      ),
+      title: Text(title, style: TextStyle(color: textColor ?? Colors.black)),
+      trailing: endIcon? Container(
+          width: 30,
+          height: 30,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(100),
+            color: Colors.grey.withOpacity(0.1),
+          ),
+          child: const Icon(LineAwesomeIcons.angle_right, size: 18.0, color: Colors.grey)) : null,
+    );
+  }
+}
