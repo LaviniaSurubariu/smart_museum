5eda8960aa32cf8e0cb4dcf94a1fbd0721e23a7c
LaviniaSurubariu <surubariu.lavinia@gmail.com>
2024-05-02 01:22:13 +0300

    add delete user freezed

---
+++ lib/actions/user_s_actions/delete_user/delete_user.freezed.dart
@@ -0,0 +1,447 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'delete_user.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$DeleteUser {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>(
+    TResult Function() $default, {
+    required TResult Function() successful,
+    required TResult Function(Object error, StackTrace stackTrace) error,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>(
+    TResult? Function()? $default, {
+    TResult? Function()? successful,
+    TResult? Function(Object error, StackTrace stackTrace)? error,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>(
+    TResult Function()? $default, {
+    TResult Function()? successful,
+    TResult Function(Object error, StackTrace stackTrace)? error,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>(
+    TResult Function(DeleteUserStart value) $default, {
+    required TResult Function(DeleteUserSuccessful value) successful,
+    required TResult Function(DeleteUserError value) error,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>(
+    TResult? Function(DeleteUserStart value)? $default, {
+    TResult? Function(DeleteUserSuccessful value)? successful,
+    TResult? Function(DeleteUserError value)? error,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>(
+    TResult Function(DeleteUserStart value)? $default, {
+    TResult Function(DeleteUserSuccessful value)? successful,
+    TResult Function(DeleteUserError value)? error,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $DeleteUserCopyWith<$Res> {
+  factory $DeleteUserCopyWith(
+          DeleteUser value, $Res Function(DeleteUser) then) =
+      _$DeleteUserCopyWithImpl<$Res, DeleteUser>;
+}
+
+/// @nodoc
+class _$DeleteUserCopyWithImpl<$Res, $Val extends DeleteUser>
+    implements $DeleteUserCopyWith<$Res> {
+  _$DeleteUserCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+}
+
+/// @nodoc
+abstract class _$$DeleteUserStartImplCopyWith<$Res> {
+  factory _$$DeleteUserStartImplCopyWith(_$DeleteUserStartImpl value,
+          $Res Function(_$DeleteUserStartImpl) then) =
+      __$$DeleteUserStartImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$DeleteUserStartImplCopyWithImpl<$Res>
+    extends _$DeleteUserCopyWithImpl<$Res, _$DeleteUserStartImpl>
+    implements _$$DeleteUserStartImplCopyWith<$Res> {
+  __$$DeleteUserStartImplCopyWithImpl(
+      _$DeleteUserStartImpl _value, $Res Function(_$DeleteUserStartImpl) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$DeleteUserStartImpl implements DeleteUserStart {
+  const _$DeleteUserStartImpl();
+
+  @override
+  String toString() {
+    return 'DeleteUser()';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$DeleteUserStartImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>(
+    TResult Function() $default, {
+    required TResult Function() successful,
+    required TResult Function(Object error, StackTrace stackTrace) error,
+  }) {
+    return $default();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>(
+    TResult? Function()? $default, {
+    TResult? Function()? successful,
+    TResult? Function(Object error, StackTrace stackTrace)? error,
+  }) {
+    return $default?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>(
+    TResult Function()? $default, {
+    TResult Function()? successful,
+    TResult Function(Object error, StackTrace stackTrace)? error,
+    required TResult orElse(),
+  }) {
+    if ($default != null) {
+      return $default();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>(
+    TResult Function(DeleteUserStart value) $default, {
+    required TResult Function(DeleteUserSuccessful value) successful,
+    required TResult Function(DeleteUserError value) error,
+  }) {
+    return $default(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>(
+    TResult? Function(DeleteUserStart value)? $default, {
+    TResult? Function(DeleteUserSuccessful value)? successful,
+    TResult? Function(DeleteUserError value)? error,
+  }) {
+    return $default?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>(
+    TResult Function(DeleteUserStart value)? $default, {
+    TResult Function(DeleteUserSuccessful value)? successful,
+    TResult Function(DeleteUserError value)? error,
+    required TResult orElse(),
+  }) {
+    if ($default != null) {
+      return $default(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class DeleteUserStart implements DeleteUser {
+  const factory DeleteUserStart() = _$DeleteUserStartImpl;
+}
+
+/// @nodoc
+abstract class _$$DeleteUserSuccessfulImplCopyWith<$Res> {
+  factory _$$DeleteUserSuccessfulImplCopyWith(_$DeleteUserSuccessfulImpl value,
+          $Res Function(_$DeleteUserSuccessfulImpl) then) =
+      __$$DeleteUserSuccessfulImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$DeleteUserSuccessfulImplCopyWithImpl<$Res>
+    extends _$DeleteUserCopyWithImpl<$Res, _$DeleteUserSuccessfulImpl>
+    implements _$$DeleteUserSuccessfulImplCopyWith<$Res> {
+  __$$DeleteUserSuccessfulImplCopyWithImpl(_$DeleteUserSuccessfulImpl _value,
+      $Res Function(_$DeleteUserSuccessfulImpl) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$DeleteUserSuccessfulImpl implements DeleteUserSuccessful {
+  const _$DeleteUserSuccessfulImpl();
+
+  @override
+  String toString() {
+    return 'DeleteUser.successful()';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$DeleteUserSuccessfulImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>(
+    TResult Function() $default, {
+    required TResult Function() successful,
+    required TResult Function(Object error, StackTrace stackTrace) error,
+  }) {
+    return successful();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>(
+    TResult? Function()? $default, {
+    TResult? Function()? successful,
+    TResult? Function(Object error, StackTrace stackTrace)? error,
+  }) {
+    return successful?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>(
+    TResult Function()? $default, {
+    TResult Function()? successful,
+    TResult Function(Object error, StackTrace stackTrace)? error,
+    required TResult orElse(),
+  }) {
+    if (successful != null) {
+      return successful();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>(
+    TResult Function(DeleteUserStart value) $default, {
+    required TResult Function(DeleteUserSuccessful value) successful,
+    required TResult Function(DeleteUserError value) error,
+  }) {
+    return successful(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>(
+    TResult? Function(DeleteUserStart value)? $default, {
+    TResult? Function(DeleteUserSuccessful value)? successful,
+    TResult? Function(DeleteUserError value)? error,
+  }) {
+    return successful?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>(
+    TResult Function(DeleteUserStart value)? $default, {
+    TResult Function(DeleteUserSuccessful value)? successful,
+    TResult Function(DeleteUserError value)? error,
+    required TResult orElse(),
+  }) {
+    if (successful != null) {
+      return successful(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class DeleteUserSuccessful implements DeleteUser {
+  const factory DeleteUserSuccessful() = _$DeleteUserSuccessfulImpl;
+}
+
+/// @nodoc
+abstract class _$$DeleteUserErrorImplCopyWith<$Res> {
+  factory _$$DeleteUserErrorImplCopyWith(_$DeleteUserErrorImpl value,
+          $Res Function(_$DeleteUserErrorImpl) then) =
+      __$$DeleteUserErrorImplCopyWithImpl<$Res>;
+  @useResult
+  $Res call({Object error, StackTrace stackTrace});
+}
+
+/// @nodoc
+class __$$DeleteUserErrorImplCopyWithImpl<$Res>
+    extends _$DeleteUserCopyWithImpl<$Res, _$DeleteUserErrorImpl>
+    implements _$$DeleteUserErrorImplCopyWith<$Res> {
+  __$$DeleteUserErrorImplCopyWithImpl(
+      _$DeleteUserErrorImpl _value, $Res Function(_$DeleteUserErrorImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? error = null,
+    Object? stackTrace = null,
+  }) {
+    return _then(_$DeleteUserErrorImpl(
+      null == error ? _value.error : error,
+      null == stackTrace
+          ? _value.stackTrace
+          : stackTrace // ignore: cast_nullable_to_non_nullable
+              as StackTrace,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$DeleteUserErrorImpl implements DeleteUserError {
+  const _$DeleteUserErrorImpl(this.error, this.stackTrace);
+
+  @override
+  final Object error;
+  @override
+  final StackTrace stackTrace;
+
+  @override
+  String toString() {
+    return 'DeleteUser.error(error: $error, stackTrace: $stackTrace)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$DeleteUserErrorImpl &&
+            const DeepCollectionEquality().equals(other.error, error) &&
+            (identical(other.stackTrace, stackTrace) ||
+                other.stackTrace == stackTrace));
+  }
+
+  @override
+  int get hashCode => Object.hash(
+      runtimeType, const DeepCollectionEquality().hash(error), stackTrace);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$DeleteUserErrorImplCopyWith<_$DeleteUserErrorImpl> get copyWith =>
+      __$$DeleteUserErrorImplCopyWithImpl<_$DeleteUserErrorImpl>(
+          this, _$identity);
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>(
+    TResult Function() $default, {
+    required TResult Function() successful,
+    required TResult Function(Object error, StackTrace stackTrace) error,
+  }) {
+    return error(this.error, stackTrace);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>(
+    TResult? Function()? $default, {
+    TResult? Function()? successful,
+    TResult? Function(Object error, StackTrace stackTrace)? error,
+  }) {
+    return error?.call(this.error, stackTrace);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>(
+    TResult Function()? $default, {
+    TResult Function()? successful,
+    TResult Function(Object error, StackTrace stackTrace)? error,
+    required TResult orElse(),
+  }) {
+    if (error != null) {
+      return error(this.error, stackTrace);
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>(
+    TResult Function(DeleteUserStart value) $default, {
+    required TResult Function(DeleteUserSuccessful value) successful,
+    required TResult Function(DeleteUserError value) error,
+  }) {
+    return error(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>(
+    TResult? Function(DeleteUserStart value)? $default, {
+    TResult? Function(DeleteUserSuccessful value)? successful,
+    TResult? Function(DeleteUserError value)? error,
+  }) {
+    return error?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>(
+    TResult Function(DeleteUserStart value)? $default, {
+    TResult Function(DeleteUserSuccessful value)? successful,
+    TResult Function(DeleteUserError value)? error,
+    required TResult orElse(),
+  }) {
+    if (error != null) {
+      return error(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class DeleteUserError implements DeleteUser, ErrorAction {
+  const factory DeleteUserError(
+      final Object error, final StackTrace stackTrace) = _$DeleteUserErrorImpl;
+
+  Object get error;
+  StackTrace get stackTrace;
+  @JsonKey(ignore: true)
+  _$$DeleteUserErrorImplCopyWith<_$DeleteUserErrorImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
