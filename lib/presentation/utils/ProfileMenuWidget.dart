8e7ed71e1d51a06c4f5c6dcfb8fa53efd629500e
LaviniaSurubariu <surubariu.lavinia@gmail.com>
2024-05-02 01:21:53 +0300

    nothing important

--- lib/presentation/utils/ProfileMenuWidget.dart
+++ lib/presentation/utils/ProfileMenuWidget.dart
@@ -5,13 +5,13 @@ import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
 class ProfileMenuWidget extends StatelessWidget {
   const ProfileMenuWidget({
-    Key? key,
+    super.key,
     required this.title,
     required this.icon,
     required this.onPress,
     this.endIcon = true,
     this.textColor,
-  }) : super(key: key);
+  });
 
   final String title;
   final IconData icon;
